2d457b1f1ea548a05b76774f460e1b129b09db43
NUFAIH <nufaihmp@gmail.com>
2025-02-08 00:23:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () =>{
   return (
     <Router>
       <Routes>
-        <Route path='/login' element={<Login/>}/> 
+        <Route path='/login' element={<Home/>}/> 
         <Route path='/' element={<Home/>}/>
         <Route path='/login/judge/addcases' element={<Cases/>}/>
       </Routes>
